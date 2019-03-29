d2696d32c499aa14a99c401945116b3fb2a21c6e
Gu Junhua <jhgu@nao.cas.cn>
2019-03-29 21:18:04 +0800

    modified:   src/Legendre.jl

--- src/Legendre.jl
+++ src/Legendre.jl
@@ -3,6 +3,15 @@ module Legendre
 import SpecialFunctions
 import Polynomials
 
+CONV_MAT=[]
+MAX_ORD_CACHED=10
+function __init__()
+    global CONV_MAT
+    CONV_MAT=leg2poly_matrix(Float64, MAX_ORD_CACHED)
+    println("module initialized")
+end
+
+
 function log_factorial(x::T)::T where {T<:AbstractFloat}
     SpecialFunctions.lgamma(x+1)
 end
@@ -21,8 +30,26 @@ function coefficient(n::T, k::T, R::DataType)::R where {T<:Integer}
     end
 end
 
+function leg2poly_coeff(p::AbstractArray{Float64, 1})::AbstractArray{Float64,1}
+    n=length(p)
+    order=n-1
+    #if order
+    if order<=MAX_ORD_CACHED
+        return CONV_MAT[1:order+1, 1:order+1]*p
+    end
+    result=zeros(Float64, n)
+    for i in 1:n
+        for j in 0:i-1
+            result[j+1]+=p[i]*coefficient(i-1, j, T)
+        end
+    end
+    result
+end
+
+
 function leg2poly_coeff(p::AbstractArray{T, 1})::AbstractArray{T,1} where {T<:AbstractFloat}
     n=length(p)
+
     result=zeros(T, n)
     for i in 1:n
         for j in 0:i-1
@@ -32,11 +59,11 @@ function leg2poly_coeff(p::AbstractArray{T, 1})::AbstractArray{T,1} where {T<:Ab
     result
 end
 
-function leg2poly_matrix(T, order::Int)::Array{T,2}
+function leg2poly_matrix(T::DataType, order::Int)::Array{T,2}
     result=zeros(T, order+1, order+1)
     for i in 1:order+1
         for j in 0:order
-            result[i,j+1]=coefficient(i-1, j, T)
+            result[j+1,i]=coefficient(i-1, j, T)
         end
     end
     result
